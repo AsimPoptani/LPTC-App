1ce059bd4903c36292c9b8479899cf5bc691944a
Asim <asim@poptani.com>
2021-02-12 17:43:43 +0000

    Fixed UUID issue

--- LPTC/index.js
+++ LPTC/index.js
@@ -1,9 +1,12 @@
 /**
  * @format
  */
-//React native navigation 
+//React native navigation
 import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
+
+// Used for randomness
+import 'react-native-get-random-values';
 import App from './App';
 import {name as appName} from './app.json';
 
