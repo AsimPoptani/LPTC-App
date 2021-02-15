c1eb92ee8fa85c69b954a09cd14e5f57aaecd792
Asim <asim@poptani.com>
2021-02-15 10:56:46 +0000

    Added subcatgory

--- LPTC/screens/SubCategoryScreen.js
+++ LPTC/screens/SubCategoryScreen.js
@@ -1,9 +1,9 @@
-import React, {useContext} from 'react';
-import {SafeAreaView} from 'react-native';
-import {Text} from 'react-native-elements';
-import {DatabaseContext} from '../DatabaseContext';
-import {findCategoryById, findAllSubCategoriesByCategory} from '../Logic';
-export default function SubCategoryScreen({route, navigation}) {
+import React, { useContext } from 'react';
+import { FlatList, SafeAreaView } from 'react-native';
+import { Text, Button } from 'react-native-elements';
+import { DatabaseContext } from '../DatabaseContext';
+import { findCategoryById, findAllSubCategoriesByCategory } from '../Logic';
+export default function SubCategoryScreen({ route, navigation }) {
   const categoryId = route.params.id;
   const databaseContext = useContext(DatabaseContext);
   const category = findCategoryById(databaseContext.database, categoryId);
@@ -11,9 +11,15 @@ export default function SubCategoryScreen({route, navigation}) {
     databaseContext.database,
     categoryId,
   );
+  navigation.setOptions({ title: category.text })
   return (
-    <SafeAreaView style={{margin: 15}}>
-      <Text h2>{id}</Text>
+    <SafeAreaView style={{ margin: 15 }}>
+      <Text h2>{category.text}</Text>
+      <Text>Take the quiz to get more accurate idea of what benefits you are entitled too...</Text>
+      <Button title={"Take the quiz"} />
+      <Text h3>Bank of knowledge</Text>
+      <Text>Or Have a look at some of the categories under {category.text}, click one to find out more.</Text>
+      <FlatList data={SubCategories} renderItem={({ item }) => <Button title={item.text}></Button>} />
     </SafeAreaView>
   );
 }
